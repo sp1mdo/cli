5bac510fda64c61a9d9048364e81291887a53f18
sp1mdo <mdorobczynski@gmail.com>
2025-01-03 16:37:43 +0100

    Simplified the folder structure

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <fstream>
+#include <set>
+#include <cstring>
+#include <map>
+
+#include "Prompt.hpp"
+
+#define GREEN_COLOR "\033[32m"
+#define DEFAULT_COLOR "\033[0m"
+
+void callback(int id, const std::string &str)
+{
+    printf("Received : id=%d arg=[%s] \n ", id, str.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    Prompt my_prompt("WORLD");
+
+    std::ifstream file(argv[1]);
+
+    // Check if the file was successfully opened
+    if (!file.is_open())
+    {
+        fprintf(stderr, "Failed to open the file.");
+        return 1;
+    }
+ int fun_id = 0;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::transform(line.begin(), line.end(), line.begin(),  [](unsigned char c){ return std::tolower(c); });
+        my_prompt.insertMapElement(std::move(line), std::bind(callback, fun_id++, std::placeholders::_1));
+    }
+    
+   
+
+
+
+    my_prompt.updateAuxMenu("");
+
+    my_prompt.run();
+
+    return 0;
+}
